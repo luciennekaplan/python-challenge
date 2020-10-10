f5556550d17b08bc5b933f2d8e1583a24c134aa3
luciennekaplan <lucykaplan@Lucys-MacBook-Air.local>
2020-10-09 18:46:44 -0700

    Commit 1

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,11 +1,19 @@
 import os
 import csv
 
-budget_data = os.path.join('..', 'uofo-por-data-pt-09-2020-u-c', '03-Python', 'HW', 'Instructions', 'PyBank', 'Resources', 'budget_data.csv')
-
-
-
+budget_data = os.path.join('Resources', 'budget_data.csv')
 
 
 with open(budget_data, 'r') as csvfile: 
-      csvreader = csv.reader(csvfile, delimiter=',')
+     csvreader = csv.reader(csvfile, delimiter=',')
+     csvheader = next(csvreader)
+
+    Months = 0
+    Total_Value = 0 
+
+
+    for row in csvreader
+        Months += 1
+        Total_Value = 
+
+
