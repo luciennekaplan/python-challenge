f8ccfe4ac20eabadae0475e0d1ee9f56b597876c
luciennekaplan <lucykaplan@Lucys-MacBook-Air.local>
2020-10-09 19:43:02 -0700

    Commit 2

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,16 +10,27 @@ with open(budget_data, 'r') as csvfile:
 
     Months = 0
     Total_Value = 0 
-
+    Previous = 0
+    Total_Change = 0
+    First_Time = True
 
     for row in csvreader:
         Months += 1
         Total_Value += int(row[1])
         
+
+        if not First_Time:
+            Change=int(row[1]) - Previous
+            Total_Change = Total_Change + Change
+       
+        Previous = int(row[1])
+        First_Time = False
+        
     
-print("Financial Analysis")   
+print("Financial Analysis")
+print("------------------")   
 print(Months)
 print(Total_Value)
-
+print(Total_Change/(Months-1))
 
 
